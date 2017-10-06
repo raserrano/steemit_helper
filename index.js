d0810c7f13483ff082dda71ad87b71ecf20c6687
raserrano <betoseru23@gmail.com>
2017-10-06 16:52:34 -0600

    Changing variable

--- index.js
+++ index.js
@@ -56,20 +56,20 @@ wait.launchFiber(function(){
   var limit = 50;
   globalData = wait.for(steem_getSteemGlobaleProperties_wrapper);
   var accounts = wait.for(steem_getAccounts_wrapper,[process.env.ACCOUNT_NAME]);
-  var accounts = process.env.VOTING_ACCS.split(',');
-  var results_booster = wait.for(getTransfers,account[0],max,limit);
+  var accounts_to = process.env.VOTING_ACCS.split(',');
+  var results_booster = wait.for(getTransfers,accounts_to[0],max,limit);
   init_conversion();
   // debug(conversionInfo);
   var weight = calculateVoteWeight(accounts[0]);
-  startVotingProcess(account[0],results_booster,weight);
+  startVotingProcess(accounts_to[0],results_booster,weight);
 
-  var results_belly = wait.for(getTransfers,account[1],max,limit);
-  startVotingProcess(account[1],results_belly,weight);
+  var results_belly = wait.for(getTransfers,accounts_to[1],max,limit);
+  startVotingProcess(accounts_to[1],results_belly,weight);
 
-  var results_minnow = wait.for(getTransfers,account[2],max,limit);
-  startVotingProcess(account[2],results_minnow,weight);
-  // var results_belly = wait.for(getTransfers,account[1],max,limit);
-  // var results_minnowbooster = wait.for(getTransfers,account[2],max,limit);
+  var results_minnow = wait.for(getTransfers,accounts_to[2],max,limit);
+  startVotingProcess(accounts_to[2],results_minnow,weight);
+  // var results_belly = wait.for(getTransfers,accounts_to[1],max,limit);
+  // var results_minnowbooster = wait.for(getTransfers,accounts_to[2],max,limit);
 });
 
 //var max = wait.for(steem_getAccountHistory_wrapper, 10000000, 1);
