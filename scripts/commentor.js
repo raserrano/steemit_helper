5e9aaf23a89a89ce3ed46e37cd41dac8e9c25210
raserrano <betoseru23@gmail.com>
2018-01-05 08:46:57 -0600

    Increasing vote weight and commentor ammount

--- scripts/commentor.js
+++ scripts/commentor.js
@@ -9,8 +9,8 @@ wait.launchFiber(function() {
   var accounts = wait.for(
     steem_api.steem_getAccounts_wrapper,[conf.env.ACCOUNT_NAME()]
   );
-  var weight = steem_api.calculateVoteWeight(accounts[0],0.02);
-  var posts = wait.for(steem_api.steem_getPostsByTag,'introduceyourself',100);
+  var weight = steem_api.calculateVoteWeight(accounts[0],0.04);
+  var posts = wait.for(steem_api.steem_getPostsByTag,'introduceyourself',150);
   var report = utils.commentOnNewUserPost(posts,weight,accounts[0]);
   if (report.length > 0) {
     utils.generateCommentedReport(report);
