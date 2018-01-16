809b97ef558640ea7c184b5ecc587460397c7b7b
raserrano <betoseru23@gmail.com>
2018-01-16 14:27:44 -0600

    Adding followers voter using 10 lucky numbers

--- model/steem_api.js
+++ model/steem_api.js
@@ -5,7 +5,7 @@ const steem = require('steem'),
 
 var ci = new Object()
 
-steem.api.setOptions({ url: 'https://api.steemit.com' })
+steem.api.setOptions({ url: 'https://rpc.buildteam.io/' })
 module.exports = {
   getTransfers: function(name, max, limit, callback) {
     steem.api.getAccountHistory(name,max,limit,function(err, result) {
--- scripts/followers.js
+++ scripts/followers.js
@@ -9,7 +9,7 @@ wait.launchFiber(function() {
   var voter = wait.for(
     steem_api.steem_getAccounts_wrapper,[conf.env.ACCOUNT_NAME()]
   );
-  var weight = steem_api.calculateVoteWeight(voter[0],0.01);
+  var weight = steem_api.calculateVoteWeight(voter[0],0.05);
   var count = wait.for(
     steem_api.steem_getFollowersCount,
     conf.env.ACCOUNT_NAME()
@@ -45,16 +45,23 @@ wait.launchFiber(function() {
   // Random
   var min = Math.ceil(0);
   var max = Math.floor(count.follower_count);
-  var lucky = Math.floor(Math.random() * (max - min + 1)) + min;
+  var lucky = new Array();
+  var k=0;
+  while(k++<10){
+    lucky.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  console.log(lucky);
   var votes = 0;
   var followers = wait.for(utils.getFollowers);
-  for(var i = 0; i< followers.length; i++){
-    console.log('Processing '+followers[i].username);
-    var posts = wait.for(steem_api.steem_getPostsByAuthor,followers[i].username,20);
+
+
+  for(var i = 0; i< lucky.length; i++){
+    console.log('Processing '+followers[lucky[i]].username);
+    var posts = wait.for(steem_api.steem_getPostsByAuthor,followers[lucky[i]].username,20);
     console.log(posts.length);
     if(posts.length > 0){
       for(var j=0; j< posts.length;j++){
-        if(followers[i].username == posts[j].author){
+        if(followers[lucky[i]].username == posts[j].author){
           // Valid posts
           if(utils.dateDiff(posts[j].created) < (86400 * 6.5)){
             console.log('Found something to vote to');
@@ -90,10 +97,50 @@ wait.launchFiber(function() {
     }else{
       console.log('No posts for '+followers[i].username);
     }
-    if(votes >= 5){
-
-    }
   }
+  // for(var i = 0; i< followers.length; i++){
+  //   console.log('Processing '+followers[i].username);
+  //   var posts = wait.for(steem_api.steem_getPostsByAuthor,followers[i].username,20);
+  //   console.log(posts.length);
+  //   if(posts.length > 0){
+  //     for(var j=0; j< posts.length;j++){
+  //       if(followers[i].username == posts[j].author){
+  //         // Valid posts
+  //         if(utils.dateDiff(posts[j].created) < (86400 * 6.5)){
+  //           console.log('Found something to vote to');
+  //           // Not voted yet
+  //           var result = wait.for(
+  //             steem_api.steem_getContent,
+  //             posts[j].author,
+  //             posts[j].permlink
+  //           );
+  //           var voted = steem_api.verifyAccountHasVoted(
+  //             [conf.env.ACCOUNT_NAME()],
+  //             result
+  //           );
+  //           if(!voted){
+  //             steem_api.votePost(posts[j].author, posts[j].permlink, weight);
+  //             wait.for(utils.timeout_wrapper,5500);
+  //             var title = 'Thanks for your donation';
+  //             var comment = 'Congratulations @' + posts[j].author + '!';
+  //               comment += ' You have received a vote as ';
+  //               comment += 'a way to thank you for supporting my program.';
+  //             // Decide how to handle this with a form and mongodb document
+  //             steem_api.commentPost(posts[j].author, posts[j].permlink, title,comment);
+  //             wait.for(utils.timeout_wrapper,22000);
+  //             votes++;
+  //             break;
+  //           }else{
+  //             console.log(posts[j].permlink);
+  //             console.log('Already voted');
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }else{
+  //     console.log('No posts for '+followers[i].username);
+  //   }
+  // }
   // vote them and update counter per day.
   console.log('Finish report');
   process.exit();
