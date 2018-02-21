ecdf4578d19db30d1d6235e7b9b01d5992072075
raserrano <betoseru23@gmail.com>
2018-02-21 14:33:13 -0600

    Fixed followers features with more than 1 thoudsand followers

--- scripts/followers.js
+++ scripts/followers.js
@@ -9,7 +9,7 @@ wait.launchFiber(function() {
   var voter = wait.for(
     steem_api.steem_getAccounts_wrapper,[conf.env.ACCOUNT_NAME()]
   );
-  var weight = steem_api.calculateVoteWeight(voter[0],0.03);
+  var weight = 1000;
   var count = wait.for(
     steem_api.steem_getFollowersCount,
     conf.env.ACCOUNT_NAME()
@@ -17,28 +17,29 @@ wait.launchFiber(function() {
   // Verify followers 
   var processed = 0;
   while(processed < count.follower_count){
-    var max = processed + 100;
+    var max = processed + 10;
     var followers = wait.for(
       steem_api.steem_getFollowers,
       conf.env.ACCOUNT_NAME(),
       processed,
       'blog',
-      max
+      10
     );
+    var current = 0;
     while(processed < max && processed < count.follower_count){
       var user = wait.for(
-        steem_api.steem_getAccounts_wrapper,[followers[processed].follower]
+        steem_api.steem_getAccounts_wrapper,[followers[current].follower]
       );
       var rep = utils.getReputation(user[0]);
       obj = {
-        username:followers[processed].follower,
+        username:followers[current].follower,
         tier:{level:0,vote:10,vote_count:2},
         active:true,
         reputation:rep,
       }
       wait.for(
         utils.upsertFollower,
-        {username:followers[processed].follower},
+        {username:followers[current].follower},
         obj
       );
       processed++;
@@ -62,7 +63,6 @@ wait.launchFiber(function() {
   for(var i = 0; i< lucky.length; i++){
     console.log('Processing '+followers[lucky[i]].username);
     var posts = wait.for(steem_api.steem_getPostsByAuthor,followers[lucky[i]].username,20);
-    console.log(posts.length);
     if(posts.length > 0){
       for(var j=0; j< posts.length;j++){
         if(followers[lucky[i]].username == posts[j].author){
@@ -102,50 +102,6 @@ wait.launchFiber(function() {
       console.log('No posts for '+followers[i].username);
     }
   }
-  // for(var i = 0; i< followers.length; i++){
-  //   console.log('Processing '+followers[i].username);
-  //   var posts = wait.for(steem_api.steem_getPostsByAuthor,followers[i].username,20);
-  //   console.log(posts.length);
-  //   if(posts.length > 0){
-  //     for(var j=0; j< posts.length;j++){
-  //       if(followers[i].username == posts[j].author){
-  //         // Valid posts
-  //         if(utils.dateDiff(posts[j].created) < (86400 * 6.5)){
-  //           console.log('Found something to vote to');
-  //           // Not voted yet
-  //           var result = wait.for(
-  //             steem_api.steem_getContent,
-  //             posts[j].author,
-  //             posts[j].permlink
-  //           );
-  //           var voted = steem_api.verifyAccountHasVoted(
-  //             [conf.env.ACCOUNT_NAME()],
-  //             result
-  //           );
-  //           if(!voted){
-  //             steem_api.votePost(posts[j].author, posts[j].permlink, weight);
-  //             wait.for(utils.timeout_wrapper,5500);
-  //             var title = 'Thanks for your donation';
-  //             var comment = 'Congratulations @' + posts[j].author + '!';
-  //               comment += ' You have received a vote as ';
-  //               comment += 'a way to thank you for supporting my program.';
-  //             // Decide how to handle this with a form and mongodb document
-  //             steem_api.commentPost(posts[j].author, posts[j].permlink, title,comment);
-  //             wait.for(utils.timeout_wrapper,22000);
-  //             votes++;
-  //             break;
-  //           }else{
-  //             console.log(posts[j].permlink);
-  //             console.log('Already voted');
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }else{
-  //     console.log('No posts for '+followers[i].username);
-  //   }
-  // }
-  // vote them and update counter per day.
   console.log('Finish report');
   process.exit();
 });
