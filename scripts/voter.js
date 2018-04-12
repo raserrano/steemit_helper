f690e1ebc90502ffe7390fde3f3db3dc9deb062f
raserrano <betoseru23@gmail.com>
2018-04-12 17:37:33 -0600

    Revert

--- scripts/voter.js
+++ scripts/voter.js
@@ -12,15 +12,21 @@ wait.launchFiber(function() {
   var accounts_to = conf.env.VOTING_ACCS().split(',');
   for (var i = 0; i < accounts_to.length; i++) {
     utils.debug('Votes for ' + accounts_to[i]);
-    var results_to = wait.for(
-      steem_api.getTransfers,
-      accounts_to[i],
-      max,
-      limit
-    );
-    var voter = wait.for(
-      steem_api.steem_getAccounts_wrapper,[conf.env.ACCOUNT_NAME()]
-    );
+    var results_to = "";
+    try{
+      results_to = wait.for(
+        steem_api.getTransfers,
+        accounts_to[i],
+        max,
+        limit
+      );
+      var voter = wait.for(
+        steem_api.steem_getAccounts_wrapper,[conf.env.ACCOUNT_NAME()]
+      );
+    }catch(e){
+      console.log(e);
+      break;
+    }
     var weight = 500;
     utils.startVotingProcess(accounts_to[i],results_to,weight,voter[0]);
   }
